a71391fbb05984ef1c86e2dd6338441abbd91987
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2018-07-19 23:57:15 +0530

    Added initial DQN code and updated requirements

--- airsim.py
+++ airsim.py
@@ -6,3 +6,36 @@
 # and transformed to an 84X84 input to the network. 
 # (You can use other sensor modalities, and sensor inputs as well.
 # Ofcourse you’ll have to modify the code accordingly).
+
+responses = client.simGetImages([ImageRequest(0, AimSimImageType.DepthPerspective, True, False)])
+current_state = transform_input(responses)
+
+# Define six actions: breaking, straight with throttle,
+# full-left with throttle, full-right with throttle,
+# half-left with throttle, half-right with throttle
+# that an agent can execute.
+def interpret_action(action):
+    car_controls.brake = 0
+    car_controls.throttle = 1
+
+    # brake
+    if action == 0:
+        car_controls.throttle = 0
+        car_controls.brake = 1
+    # straight throttle
+    elif action == 1:
+        car_controls.steering = 0
+    # full-right throttle
+    elif action == 2:
+        car_controls.steering = 0.5
+    # full-left throttle
+    elif action == 3:
+        car_controls.steering = -0.5
+    # half-right throttle
+    elif action == 4:
+        car_controls.steering = 0.25
+    # half-left throttle
+    else:
+        car_controls.steering = -0.25
+
+    return car_controls       
